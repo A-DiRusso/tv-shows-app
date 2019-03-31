f1999ddf2f44f4e655663e134f487a30d35d49f4
Anthony DiRusso <ossurid@gmail.com>
2019-03-31 10:44:35 -0400

    adds db values

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
 --schema.sql--
+create table worst_tv_shows (
+    id serial primary key,
+    show_name varchar(300),
+    station varchar(300),
+    year_start integer,
+    year_end integer
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,10 @@
 --seed.sql--
+insert into worst_tv_shows
+    (show_name, station, year_start, year_end)
+values
+    ('The Jerry Springer Show', 'syndication', 1991, 2222),
+    ('My Mother The Car', 'NBC', 1965, 1966),
+    ('XFL', 'NBC, UPN, TNN', 2001, 2001),
+    ('The Brady Bunch Hour', 'ABC', 2001, 2001),
+    ('Hogan''s Heros', 'CBS', 1965, 1971)
+;
